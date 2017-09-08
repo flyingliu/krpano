48f50ba701d048252d09732acf3335c24212393f
flyngliu <fly0733@gmail.com>
2017-09-08 18:01:47 +0800

    sda

--- js/enter.js
+++ js/enter.js
@@ -33,6 +33,17 @@ require(mods, function($, embedpano, Pano, testa, testb) {
     let a = new testa();
     abc = a.init();
     console.log(abc);
+    $.ajax("http://lbsyun.baidu.com/data/poi/list?geotable_id=45970&page_size=10&page_index=0&sn=be2afa3d9384f8061a3b3ef348f5dad1&i=1504860672333&_=1504860669872", {
+            dataType: 'jsonp'
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+
+
+
+
+
     pano = new Pano({
         xml: 'pano/1.xml'
     })
