c34eb07dd0311d3f0a3854fbd64df7b8761bcff3
flyngliu <fly0733@gmail.com>
2017-11-20 11:02:31 +0800

    12

---
+++ newjs/ui.js
@@ -0,0 +1,53 @@
+//  js/script/main.js
+
+var pano;
+require.config({
+    paths: {
+        'jquery': 'https://cdn.bootcss.com/jquery/3.2.1/jquery.min',
+        'async': 'https://cdn.bootcss.com/async/2.4.1/async',
+        'mymap': '/components/bmap',
+        'data': '/js/data',
+        'axios': 'https://cdn.bootcss.com/axios/0.16.2/axios.min',
+        'vue': 'https://cdn.bootcss.com/vue/2.4.2/vue.min',
+        'iscroll':'https://cdn.bootcss.com/iScroll/5.2.0/iscroll-zoom.min',
+        'layer': 'js/layer/layer',
+        'embedpano': '/krpano/embedpano',
+        'krpano': '/krpano/krpano',
+        'pano': '/js/main',
+        'testa': '/plugin/testa',
+        'testb': '/plugin/testb',
+        'foo': '/plugin/foo',
+        'bar': '/plugin/bar'
+    },
+    shim: {
+        'layer': {
+            deps: ['jquery'],
+            exports: 'layer'
+        }
+    }
+})
+
+var mods = [
+    'jquery',
+    'vue',
+    'mymap',
+    'data'
+]
+
+require(mods, function($, Vue, mymap, Data) {
+    const plugin = {};
+    var abc;
+    $(() => {
+        let vm = new Vue({
+            el: "#app",
+            data: {
+                mydata: Data.data[0].nodeList[0].vrList,
+                mapinit: Data.data[0].map
+            },
+            components: {
+                'my-map': mymap
+            }
+        })
+    })
+
+})
